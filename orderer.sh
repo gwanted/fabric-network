8636f9604d687849855d5124bbaece122b48c8d8
zhangqichao <zhangqichao@gjk365.com>
2019-01-23 17:42:15 +0800

    commit

--- orderer.sh
+++ orderer.sh
@@ -2,14 +2,14 @@ MODE=$1
 SERVER=$2
 if [ "${MODE}" == "zookeeper" ]; then
 cd zookeeper
-docker-compose -f zookeeper{$SERVER}.ulaw.com.yaml down
-docker-compose -f zookeeper{$SERVER}.ulaw.com.yaml up -d
+docker-compose -f zookeeper$SERVER.ulaw.com.yaml down
+docker-compose -f zookeeper$SERVER.ulaw.com.yaml up -d
 elif [ "${MODE}" == "kafka" ]; then
 cd kafka
-docker-compose -f kafka{$SERVER}.ulaw.com.yaml down
-docker-compose -f kafka{$SERVER}.ulaw.com.yaml up -d
+docker-compose -f kafka$SERVER.ulaw.com.yaml down
+docker-compose -f kafka$SERVER.ulaw.com.yaml up -d
 elif [ "${MODE}" == "orderer" ]; then
 cd orderer
-docker-compose -f orderer{$SERVER}.ulaw.com.yaml down
-docker-compose -f orderer{$SERVER}.ulaw.com.yaml up -d
+docker-compose -f orderer$SERVER.ulaw.com.yaml down
+docker-compose -f orderer$SERVER.ulaw.com.yaml up -d
 fi
--- peer.sh
+++ peer.sh
@@ -1,22 +1,22 @@
 SERVER=$1
 ORG=$2
 cd peer
-docker-compose -f peer{$SERVER}.org{$ORG}.ulaw.com.yaml down
-docker-compose -f peer{$SERVER}.org{$ORG}.ulaw.com.yaml up -d
+docker-compose -f peer$SERVER.org$ORG.ulaw.com.yaml down
+docker-compose -f peer$SERVER.org$ORG.ulaw.com.yaml up -d
 
-if [ ${SERVER} == 0 && ${ORG} == 1]; then
+if [ $SERVER == 0 && $ORG == 1]; then
 cd couchdb
 docker-compose -f couchdb1.ulaw.com.yaml down
 docker-compose -f couchdb1.ulaw.com.yaml up -d
-elif [ ${SERVER} == 1 && ${ORG} == 1]; then
+elif [ $SERVER == 1 && $ORG == 1]; then
 cd couchdb
 docker-compose -f couchdb2.ulaw.com.yaml down
 docker-compose -f couchdb2.ulaw.com.yaml up -d
-elif [ ${SERVER} == 0 && ${ORG} == 2]; then
+elif [ $SERVER == 0 && $ORG == 2]; then
 cd couchdb
 docker-compose -f couchdb3.ulaw.com.yaml down
 docker-compose -f couchdb3.ulaw.com.yaml up -d
-elif [ ${SERVER} == 1 && ${ORG} == 2]; then
+elif [ $SERVER == 1 && $ORG == 2]; then
 cd couchdb
 docker-compose -f couchdb4.ulaw.com.yaml down
 docker-compose -f couchdb4.ulaw.com.yaml up -d
