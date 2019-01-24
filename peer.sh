a7d3cf5582ab7e83afcf1c85b823e113e6807709
zhangqichao <zhangqichao@gjk365.com>
2019-01-24 09:30:31 +0800

    commit

--- peer.sh
+++ peer.sh
@@ -1,24 +1,21 @@
-SERVER=$1
-ORG=$2
+ORG=$1
 export IMAGE_TAG=latest
 cd peer
-docker-compose -f peer$SERVER.org$ORG.ulaw.com.yaml down
-docker-compose -f peer$SERVER.org$ORG.ulaw.com.yaml up -d
+docker-compose -f peer0.org$ORG.ulaw.com.yaml down
+docker-compose -f peer0.org$ORG.ulaw.com.yaml up -d
+docker-compose -f peer1.org$ORG.ulaw.com.yaml down
+docker-compose -f peer1.org$ORG.ulaw.com.yaml up -d
 
-if [ $SERVER == 0 && $ORG == 1]; then
+if [ "$ORG" == "1" ]; then
 cd couchdb
 docker-compose -f couchdb1.ulaw.com.yaml down
 docker-compose -f couchdb1.ulaw.com.yaml up -d
-elif [ $SERVER == 1 && $ORG == 1]; then
-cd couchdb
 docker-compose -f couchdb2.ulaw.com.yaml down
 docker-compose -f couchdb2.ulaw.com.yaml up -d
-elif [ $SERVER == 0 && $ORG == 2]; then
+elif [ "$ORG" == "2" ]; then
 cd couchdb
 docker-compose -f couchdb3.ulaw.com.yaml down
 docker-compose -f couchdb3.ulaw.com.yaml up -d
-elif [ $SERVER == 1 && $ORG == 2]; then
-cd couchdb
 docker-compose -f couchdb4.ulaw.com.yaml down
 docker-compose -f couchdb4.ulaw.com.yaml up -d
 fi
