dbee7a5f8897810f25171b6fb9bfe82acfd0b0d4
zhangqichao <zhangqichao@gjk365.com>
2019-01-24 10:19:52 +0800

    commit

--- peer.sh
+++ peer.sh
@@ -2,19 +2,19 @@ ORG=$1
 export IMAGE_TAG=latest
 cd peer
 docker-compose -f peer0.org$ORG.ulaw.com.yaml down
-docker-compose -f peer0.org$ORG.ulaw.com.yaml up -d
 docker-compose -f peer1.org$ORG.ulaw.com.yaml down
+docker-compose -f peer0.org$ORG.ulaw.com.yaml up -d
 docker-compose -f peer1.org$ORG.ulaw.com.yaml up -d
 
 cd ../couchdb
 if [ "$ORG" == "1" ]; then
 docker-compose -f couchdb1.ulaw.com.yaml down
-docker-compose -f couchdb1.ulaw.com.yaml up -d
 docker-compose -f couchdb2.ulaw.com.yaml down
+docker-compose -f couchdb1.ulaw.com.yaml up -d
 docker-compose -f couchdb2.ulaw.com.yaml up -d
 elif [ "$ORG" == "2" ]; then
 docker-compose -f couchdb3.ulaw.com.yaml down
-docker-compose -f couchdb3.ulaw.com.yaml up -d
 docker-compose -f couchdb4.ulaw.com.yaml down
+docker-compose -f couchdb3.ulaw.com.yaml up -d
 docker-compose -f couchdb4.ulaw.com.yaml up -d
 fi
