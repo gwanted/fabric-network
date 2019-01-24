266700d92fdedde24ab64c63d610395deb679ab6
zhangqichao <zhangqichao@gjk365.com>
2019-01-24 09:35:47 +0800

    commit

--- peer.sh
+++ peer.sh
@@ -6,14 +6,13 @@ docker-compose -f peer0.org$ORG.ulaw.com.yaml up -d
 docker-compose -f peer1.org$ORG.ulaw.com.yaml down
 docker-compose -f peer1.org$ORG.ulaw.com.yaml up -d
 
+cd ../couchdb
 if [ "$ORG" == "1" ]; then
-cd couchdb
 docker-compose -f couchdb1.ulaw.com.yaml down
 docker-compose -f couchdb1.ulaw.com.yaml up -d
 docker-compose -f couchdb2.ulaw.com.yaml down
 docker-compose -f couchdb2.ulaw.com.yaml up -d
 elif [ "$ORG" == "2" ]; then
-cd couchdb
 docker-compose -f couchdb3.ulaw.com.yaml down
 docker-compose -f couchdb3.ulaw.com.yaml up -d
 docker-compose -f couchdb4.ulaw.com.yaml down
